dc3bf034204e2fca786664f8d1aa587f5b3a7ce3
almolhamho-ai <almolham.ho@gmail.com>
2026-08-19 18:01:59 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.almolham.bingo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 41
-        versionName = "0.41"
+        versionCode = 44
+        versionName = "0.44"
     }
 
     signingConfigs {
